973df0fd9773489104a0a0c1d8f11543d230c8f4
Patrik Vas <vas.patrik.d@gmail.com>
2020-12-07 19:50:20 +0200

    Add initial functinality

--- app.py
+++ app.py
@@ -1,31 +1,80 @@
-from flask import Flask, render_template, request
-from urllib.request import urlopen
 import threading
 import queue
-
-from bs4 import BeautifulSoup as bs
 from threading import Thread
 
-app = Flask(__name__)
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
+from urllib.request import urlopen
+from bs4 import BeautifulSoup as bs
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///price-cmp.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Competitor(db.Model):
+	username = db.Column(db.String(200), primary_key=True, nullable=False)
+	comment = db.Column(db.String(255))
+
+class Search(db.Model):
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	search = db.Column(db.String(200), nullable=False)
+	price = db.Column(db.Numeric(10,2))
+	tacked_products = db.relationship('TrackedProduct', backref='Seach', lazy=True)
+
+
+class TrackedProduct(db.Model):
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	name = db.Column(db.String(200), nullable=False)
+	product_id = db.Column(db.String(200), nullable=False)
+	seller = db.Column(db.String(200), nullable=False)
+	url = db.Column(db.String(1200), nullable=False)
+	shipping = db.Column(db.String(1200), nullable=False)
+	price = db.Column(db.Numeric(10,2))
+	search_id = db.Column(db.Integer, db.ForeignKey('search.id'), nullable=False)
 
 
 @app.route('/')
-def hello_world():
+@app.route('/new_product')
+def index():
 	return render_template('index.html')
 
-@app.route('/new-product', methods=['POST'])
-def new_product():
+@app.route('/view_tracked_products')
+def view_tracked_products():
+	trackedProducts = Search.query.all()
+	return render_template('view_tracked_products.html', products=trackedProducts)
+
+
+@app.route('/competitors', methods=['POST', 'GET'])
+def competitors():
+	if request.method == 'POST':
+		username = request.form['username']
+		comment = request.form['comment']
+		c = Competitor(username=username, comment=comment)
+		db.session.add(c)
+		db.session.commit()
+	comps = Competitor.query.all()
+	return render_template('competitors.html', competitors=comps)
+
+
+@app.route('/save-search', methods=['POST'])
+def save_search():
 	query = request.form['product']
-	products = get_related_products(query)
+	products = save_related_products(query)
 	return render_template('related_products.html', query=query, products=products)
 
+@app.route('/related_products', methods=['GET'])
+def search_products():
+	sid = request.args.get('search_id')
+	search = Search.query.filter_by(id=sid).first()
+	return render_template('related_products.html', search=search)
 
-def get_related_products(query):
+
+def save_related_products(query):
 	search_url = 'https://www.trademe.co.nz/Browse/SearchResults.aspx?buy=buynow&v=List&searchString=' + query.replace(' ', '+')
 	page_soup = bs(urlopen(search_url).read(), 'html.parser')
-	totalCount = page_soup.find(id="totalCount").text
-	
+	total_count = page_soup.find(id="totalCount").text
 	result = queue.Queue()
 	threads = [ getProcessProductItemThreads(item, result) for item in page_soup.find_all('div', attrs={'data-listingid' : True})]
 	
@@ -34,24 +83,29 @@ def get_related_products(query):
 	for t in threads:
 		t.join()
 
-	return list(result.queue)
+	search = Search(search=query, price=0)
+	search.tacked_products = list(result.queue)
+	db.session.add(search)
+	db.session.commit()
+
+	return search.tacked_products
 
 def get_product_data(url, product, result):
 	page_content = bs(urlopen(url).read(), 'html.parser')
 	seller = page_content.find('a', {'class': 'seller-name'}).text
 	shippingUL = page_content.find('ul', {'id': 'ShippingDetails_CustomShippingOptionList'})
 	shippingPrices = '/'.join([sp.text for sp in shippingUL.findAll('span', {'class': 'custom-shipping-price'})])
-	product['seller'] =  seller
-	product['shipping'] = shippingPrices
+	product.seller =  seller
+	product.shipping = shippingPrices
 	result.put(product)
 
 def getProcessProductItemThreads(item, result):
 	id = item['data-listingid']
-	title = item.find('div', {'class': 'title'}).text.strip()
-	price = item.find('div', {'class': 'listingBuyNowPrice'}).text.strip()
-	link = 'https://www.trademe.co.nz/' + item.parent['href'].split('?')[0]
-	product = {'id': id, 'title': title, 'price': price, 'link': link}
-	return Thread(target=get_product_data, args = (link, product, result))
+	name = item.find('div', {'class': 'title'}).text.strip()
+	price = item.find('div', {'class': 'listingBuyNowPrice'}).text.strip()[1:]
+	url = 'https://www.trademe.co.nz/' + item.parent['href'].split('?')[0]
+	product = TrackedProduct(product_id=id, name=name, price=price, url=url)
+	return Thread(target=get_product_data, args = (url, product, result))
 
 if __name__ == '__main__':
 	app.run(debug=True)
